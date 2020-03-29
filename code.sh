19aced5dbf81bcf512c2654abc4a2d870f5c1753
Satnam Singh <sonuthandi85@gmail.com>
2020-03-29 14:23:32 -0700

    adding code flow for oAuth with Google

---
+++ code.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+APP_ID=''
+APP_SECRET=''
+URI='https://ssingh.wmdd4950.com/oauth/'
+
+echo "content-type: application/json"
+echo ""
+
+CODE=$QUERY_STRING
+
+# get token from OAuth provider using the code got the index.html page
+
+curl POST -d  "code=${CODE}&redirect_uri=${URI}&client_id=${APP_ID}&client_secret=${APP_SECRET}&grant_type=authorization_code" https://oauth2.googleapis.com/token
+
+# Notes
+# Add code.sh in cgi-bin
+# get app id , app secret Key
