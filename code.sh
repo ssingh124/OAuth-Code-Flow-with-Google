ebde90d656028f1acfd14dfc7cc5d84ee67e2393
Satnam Singh <sonuthandi85@gmail.com>
2020-03-29 14:57:32 -0700

    commit

--- code.sh
+++ code.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-APP_ID=''
-APP_SECRET=''
+APP_ID='885522048871-fhhejrcho5gk2hfm5gbhq8tmdvusl7nn.apps.googleusercontent.com'
+APP_SECRET='JFe3sty9y7nbtA_0LGvp68JQ'
 URI='https://ssingh.wmdd4950.com/oauth/'
 
 echo "content-type: application/json"
@@ -9,8 +9,10 @@ echo ""
 
 CODE=$QUERY_STRING
 
-# get token from OAuth provider using the code got the index.html page
 
+
+
+# get token from OAuth provider using the code got the index.html page
 curl POST -d  "code=${CODE}&redirect_uri=${URI}&client_id=${APP_ID}&client_secret=${APP_SECRET}&grant_type=authorization_code" https://oauth2.googleapis.com/token
 
 # Notes
